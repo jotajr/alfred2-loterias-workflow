8b09f642270295d35a9f7691f71d526e6b591b64
Jota Freitas Jr <jota.freitas@embedded.ufcg.edu.br>
2015-10-28 16:57:29 -0300

    Adicionando mais resultados.

--- src/loterias.py
+++ src/loterias.py
@@ -63,3 +63,105 @@ def mega_acumulada():
     result = "Acumulado: R$ " + data['concurso']['valor_acumulado']
 
     print(result)
+
+
+def quina_request():
+    response = urllib2.urlopen('http://developers.agenciaideias.com.br/loterias/quina/json')
+
+    data = json.load(response)
+
+    quina_file = "quina.json"
+
+    try:
+        os.remove(quina_file)
+    except OSError:
+        pass
+
+    with io.open(quina_file, 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(data, ensure_ascii=False)))
+
+
+def quina_concurso():
+
+    quina_request()
+
+    with open('quina.json') as data_file:
+        data = json.load(data_file)
+
+    result = "Concurso: " + data['concurso']['numero'] + " - Data: " + data['concurso']['data']
+
+    print(result)
+
+
+def quina_num_sorteados():
+
+    with open('quina.json') as data_file:
+        data = json.load(data_file)
+
+    sorteados = ""
+
+    for item in data['concurso']['numeros_sorteados']:
+        sorteados = sorteados + str(item) + " "
+
+    print(sorteados)
+
+
+def quina_ganhadores():
+
+    with open('quina.json') as data_file:
+        data = json.load(data_file)
+
+    result = "Ganhadores: " + data['concurso']['premiacao']['quina']['ganhadores']
+
+    print(result)
+
+
+def quina_acumulada():
+
+    with open('mega.json') as data_file:
+        data = json.load(data_file)
+
+    result = "Acumulado: R$ " + data['concurso']['valor_acumulado']
+
+    print(result)
+
+
+def federal_request():
+    response = urllib2.urlopen('http://developers.agenciaideias.com.br/loterias/loteriafederal/json')
+
+    data = json.load(response)
+
+    fed_file = "federal.json"
+
+    try:
+        os.remove(fed_file)
+    except OSError:
+        pass
+
+    with io.open(fed_file, 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(data, ensure_ascii=False)))
+
+
+def federal_concurso():
+    federal_request()
+
+    with open('federal.json') as data_file:
+        data = json.load(data_file)
+
+    result = "Concurso: " + data['concurso']['numero'] + " - Data: " + data['concurso']['data']
+
+    print(result)
+
+
+def federal_premios():
+
+    with open('federal.json') as data_file:
+        data = json.load(data_file)
+
+    result = " 1 - " + data['concurso']['premiacao']['premio_1']['bilhete'] + " - "
+    result += "2 - " + data['concurso']['premiacao']['premio_2']['bilhete'] + " - "
+    result += "3 - " + data['concurso']['premiacao']['premio_3']['bilhete'] + " - "
+    result += "4 - " + data['concurso']['premiacao']['premio_4']['bilhete'] + " - "
+    result += "5 - " + data['concurso']['premiacao']['premio_5']['bilhete']
+
+    print(result)
--- src/loterias.sh
+++ src/loterias.sh
@@ -1,15 +1,33 @@
-CONCURSO=$(python -c "import loterias; loterias.mega_concurso()")
+CONCURSO_MEGA=$(python -c "import loterias; loterias.mega_concurso()")
 MEGA=$(python -c "import loterias; loterias.mega_num_sorteados()")
-GANHADORES=$(python -c "import loterias; loterias.mega_ganhadores()")
-ACUMULADO=$(python -c "import loterias; loterias.mega_acumulada()")
+GANHADORES_MEGA=$(python -c "import loterias; loterias.mega_ganhadores()")
+ACUMULADO_MEGA=$(python -c "import loterias; loterias.mega_acumulada()")
+
+CONCURSO_QUINA=$(python -c "import loterias; loterias.quina_concurso()")
+QUINA=$(python -c "import loterias; loterias.quina_num_sorteados()")
+GANHADORES_QUINA=$(python -c "import loterias; loterias.quina_ganhadores()")
+ACUMULADO_QUINA=$(python -c "import loterias; loterias.quina_acumulada()")
+
+CONCURSO_FEDERAL=$(python -c "import loterias; loterias.federal_concurso()")
+FEDERAL=$(python -c "import loterias; loterias.federal_premios()")
 
 cat << EOB
 <?xml version="1.0"?>
 <items>
   <item>
-    <title>Mega Sena - $CONCURSO</title>
-	  <subtitle>Números: $MEGA- $GANHADORES - $ACUMULADO</subtitle>
+    <title>Mega Sena - $CONCURSO_MEGA</title>
+	  <subtitle>Números: $MEGA- $GANHADORES_MEGA - $ACUMULADO_MEGA</subtitle>
     <icon>mega_sena_icon.png</icon>
+  </item>
+  <item>
+    <title>Quina - $CONCURSO_QUINA</title>
+	  <subtitle>Números: $QUINA- $GANHADORES_QUINA - $ACUMULADO_QUINA</subtitle>
+    <icon>quina_icon.png</icon>
+  </item>
+  <item>
+    <title>Loteria Federal - $CONCURSO_FEDERAL</title>
+	  <subtitle>Prêmios: $FEDERAL</subtitle>
+    <icon>federal_icon.png</icon>
   </item>
 </items>
 EOB
